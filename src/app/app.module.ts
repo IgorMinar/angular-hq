a19eda604377e901095a35bd1c0cfb093bbfa0cd
Igor Minar <igor@angular.io>
2017-11-13 18:44:22 +0100

    initial version that works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {GithubService} from "./github.service";
 
 @NgModule({
   declarations: [
@@ -10,7 +11,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [GithubService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  ghAuthToken: "96fe7f207a604d0847ea6cdab0029e9b26d404b9" //ngdashboard token with public-only and read-only permissions
+              //"9eb2fc92bdc3a07a69bd21b42bb2a1c006ebc0f7"
 };
